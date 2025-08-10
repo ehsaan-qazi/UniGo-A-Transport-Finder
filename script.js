92dcfd32d7dc3eb357b3371ef22f9e75cb7a762a
ehsaan-qazi <ehsaanqazi27@gmail.com>
2025-08-10 17:52:50 +0500

    just UI

---
+++ script.js
@@ -0,0 +1,13 @@
+let data=[
+    ["comsats","comsats","No transport needed"],
+    ["saddar","comsats","Metro bus"]
+]
+function findTrans(){
+    let value = document.getElementById("departure-dropdown").value;
+    let value2 = document.getElementById("destination-dropdown").value;
+    for(let element of data){
+        if(element[0]===value && element[1]===value2){
+            console.log(element[2]);
+        }
+    }
+}
